278c10bcf0ad2b842fa5629eadfa3b9c423d654d
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2018-10-03 23:13:33 +0200

    More edits to visualization script

--- visualize_metagenomes.py
+++ visualize_metagenomes.py
@@ -6,6 +6,7 @@ import matplotlib
 import argparse
 import sys
 import seaborn as sns
+from sklearn.decomposition import PCA
 
 '''this script takes the output tables from recalibrate_metagenome.py and will generate stacked barplots of content
 USAGE EXAMPLE
@@ -44,39 +45,8 @@ pivot_df.loc[:, ['Jan', 'Feb', 'Mar']].plot.bar(stacked=True, color=colors, figs
 
 plt.savefig('genus_barplot.pdf')
 
-
+# PCA
 '''
-#y-axis in bold
-rc('font', weight = 'bold')
-
-# Values of each group
-bars1 = [12, 28, 1, 8, 22]
-bars2 = [28, 7, 16, 4, 10]
-bars3 = [25, 3, 23, 25, 17]
- 
-# Heights of bars1 + bars2 (TO DO better)
-bars = [40, 35, 17, 12, 32]
- 
-# The position of the bars on the x-axis
-#r = [0,1,2,3,4]
-r = np.arange(len(bars))
- 
-# Names of group and bar width
-names = ['A','B','C','D','E']
-barWidth = 1
- 
-# Create brown bars
-plt.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=barWidth)
-# Create green bars (middle), on top of the firs ones
-plt.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=barWidth)
-# Create green bars (top)
-plt.bar(r, bars3, bottom=bars, color='#2d7f5e', edgecolor='white', width=barWidth)
- 
-# Custom X axis
-plt.xticks(r, names, fontweight='bold')
-plt.xlabel("group")
-plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-
-# Show graphic
-plt.show()
+pca = PCA(n_components=5)
+pca.fit(df)
 '''
