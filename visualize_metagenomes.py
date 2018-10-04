286e48f902d71077bf1f404439d7dbd7ce3a0769
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2018-10-04 19:19:44 +0200

    Visualization edits

--- visualize_metagenomes.py
+++ visualize_metagenomes.py
@@ -1,8 +1,9 @@
+import matplotlib as mpl
+mpl.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import pandas as pd
-import matplotlib
 import argparse
 import sys
 import seaborn as sns
@@ -10,43 +11,47 @@ from sklearn.decomposition import PCA
 
 '''this script takes the output tables from recalibrate_metagenome.py and will generate stacked barplots of content
 USAGE EXAMPLE
-./visualize_metagenome -genus_meta_table genus_recalibrate -family_meta_table family_recalibrate 
+./visualize_metagenome -genus_meta_table genus_recalibrate -family_meta_table family_recalibrate
 
 '''
 
-genus = sys.argv[0]
-family = sys.argv[1]
+genus_file = sys.argv[0]
+family_file = sys.argv[1]
+genus = pd.read_csv(genus_file, index_col=0).transpose()
 
-
+#sort genus on
 # or with a pandas dataframe
 # matplotlib.style.use('ggplot')
 # https://pstblog.com/2016/10/04/stacked-charts
-data = [[2000, 2000, 2000, 2001, 2001, 2001, 2002, 2002, 2002],
-        ['Jan', 'Feb', 'Mar', 'Jan', 'Feb', 'Mar', 'Jan', 'Feb', 'Mar'],
-        [1, 2, 3, 4, 5, 6, 7, 8, 9]]
-
-rows = list(zip(data[0], data[1], data[2]))
-headers = ['Year', 'Month', 'Value']
-df = pd.DataFrame(rows, columns=headers)
-
-df
 
 
-fig, ax = plt.subplots(figsize=(10, 7))
-months = df['Month'].drop_duplicates()
-margin_bottom = np.zeros(len(df['Year'].drop_duplicates()))
-colors = sns.color_palette("cubehelix", n_colors=len(df.columns))
-months = df['Month'].drop_duplicates()
-margin_bottom = np.zeros(len(df['Year'].drop_duplicates()))
-pivot_df = df.pivot(index='Year', columns='Month', values='Value')
+fig, ax = plt.subplots()
+#months = df['Month'].drop_duplicates()
+#margin_bottom = np.zeros(len(['Year'].drop_duplicates()))
+colors = sns.color_palette("cubehelix", n_colors=len(genus.index))
+#pivot_df = d.pivot(index='Year', columns='Month', values='Value')
 # Note: .loc[:,['Jan','Feb', 'Mar']] is used here to rearrange the layer ordering
-pivot_df.loc[:, ['Jan', 'Feb', 'Mar']].plot.bar(stacked=True, color=colors, figsize=(10, 7), width=0.95)
+genus.plot.bar(stacked=True, color=colors, figsize=(10, 7), width=0.95)
 # plt.show()
+fig.legend(loc=7, bbox_to_anchor= (1.2, 0.5))
+#fig.tight_layout()
+#fig.subplots_adjust(right=0.75)
 plt.ylabel("Proportion of genome covered per A. thaliana genome")
-plt.savefig('genus_barplot.pdf')
+plt.savefig('genus_barplot.pdf', bbox_inches="tight")
 
 # PCA
 '''
 pca = PCA(n_components=5)
 pca.fit(df)
 '''
+
+
+
+c = ["blue", "purple", "red", "green", "pink"]
+for i, g in enumerate(genus):
+    ax = sns.barplot(data=genus[g],
+                     #hue="Name",
+                     color=colors,
+                     zorder=-i,
+                     edgecolor="k")
+ax.legend_.remove() # remove the redundant legends
