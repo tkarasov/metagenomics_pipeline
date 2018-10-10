a127242f11c4c8798f5eac69a0a94a79a546b4bd
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2018-10-10 18:36:35 +0200

    Trying lotka-voltera stuff

---
+++ generalized_lotka_voltera.R
@@ -0,0 +1,28 @@
+library(deSolve)
+library(lattice)
+
+deltax = mu+
+  
+  
+predpreyLV<-function(t,y,p){
+  N<-y[1]
+  p<-y[2]
+  
+}
+
+LotVmod <- function (Time, State, Pars) {
+  with(as.list(c(State, Pars)), {
+    dx = x*(alpha - beta*y)
+    dy = -y*(gamma - delta*x)
+    return(list(c(dx, dy)))
+  })
+}
+
+Pars <- c(alpha = 2, beta = .5, gamma = .2, delta = .6)
+State <- c(x = 10, y = 10)
+Time <- seq(0, 100, by = 1)
+
+out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time))
+
+matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
+legend("topright", c("Cute bunnies", "Rabid foxes"), lty = c(1,2), col = c(1,2), box.lwd = 0)
