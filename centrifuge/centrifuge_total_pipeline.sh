ff1e69b0a4f4ee93ed60bd4a170e8a767e48d7ba
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2019-04-24 17:07:11 +0200

    Edited for fung and oom

--- centrifuge/centrifuge_total_pipeline.sh
+++ centrifuge/centrifuge_total_pipeline.sh
@@ -1,28 +1,30 @@
-#!/bin/bash
+#!/bin/sh
+#
 #  Reserve 4 CPUs for this job
-#$ -pe parallel 6
-#  Request 128G of RAM
-#$ -l h_vmem=32G
-#$ -cwd
-#$ -e error_centrifuge_total.out
-#$ -o output_centrifuge_total.out
+#$ -pe parallel 4
+#  Request 32G of RAM
+#$ -l h_vmem=8G
+#$ -o $HOME/tmp/stdout_of_job
 #  The name shown in the qstat output and in the output file(s). The
 #  default is to use the script name.
 #$ -N metagenome.$1
 #  Run job from current working directory
 # Merge stdout and stderr. The job will create only one output file which
-#  contains both the real output and the error messages.
+# contains both the real output and the error messages.
+#$ -e error_centrifuge_total.out
+#$ -o output_centrifuge_total.out
 #$ -j y
 #  Use /bin/bash to execute this script
 #$ -S /bin/bash
+#
 
-
-#usage qsub -v curr_direc=/ebio/abt6_projects9/metagenomic_controlled/data/processed_reads/dc3000_infections/ /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/centrifuge_total_pipeline.sh
+# usage qsub -v curr_direc=/ebio/abt6_projects9/metagenomic_controlled/data/processed_reads/dc3000_infections/ /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/centrifuge_total_pipeline.sh
 
 
 #centrifuge pipeline instructions
 #change to the directory of reads
 curr_direc=$curr_direc
+cd $curr_direc
 echo "This is the directory:"$curr_direc
 python=/ebio/abt6_projects7/small_projects/tkarasov/Programs/miniconda3/bin/python
 
@@ -31,14 +33,15 @@ python=/ebio/abt6_projects7/small_projects/tkarasov/Programs/miniconda3/bin/pyth
 #run_plantRemoval_tlk_centrifuge.sh
 
 #2 Take unmapped reads and classify with centrifuge
-bash /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/centrifuge_db.sh $curr_direc
+#bash /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/centrifuge_db.sh $curr_direc
+
+
 #3 once all classification is done, aggregate output from centrifuge and run process_centrifuge.
 # The metagenomic_report file has a list of all of the centrifuge output reports
 ls $curr_direc/centrifuge_output/*R1.fq.report > $curr_direc/centrifuge_output/metagenomic_report.txt
 
-#$python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/process_centrifuge.py
+# $python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/process_centrifuge.py
 $python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/classify_eukaryote_prokaryote.py
-
 
 
 #4 Feed output from #3 into recalibrate which outputs normalized data file
@@ -50,4 +53,4 @@ $python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeli
 
 
 #5 Graph output from #4
-$python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/visualize_metagenomes.py
+# $python /ebio/abt6_projects9/metagenomic_controlled/Programs/metagenomics_pipeline/centrifuge/visualize_metagenomes.py
