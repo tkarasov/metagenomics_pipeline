c422fa1d2799febcade595e3bec826bfa79fe81f
Talia Karasov <talia.karasov@tuebingen.mpg.de>
2019-06-04 15:00:23 +0200

    script to select for optimal coverage

---
+++ filter_genomes_on_coverage.sh
@@ -0,0 +1,43 @@
+
+#this script counted the number of reads in each file and output. I then filtered for those with more than 30,000 reads.
+
+for direc in `ls /ebio/abt6_projects9/metagenomic_controlled/data/raw_reads/swedish_samples`; do
+echo $direc
+my_fil=`ls $direc | grep R2`
+for rec in $my_fil
+do
+num_rec=`zcat $direc/$rec | grep "^@" | wc -l`
+echo $direc/rec, $num_rec >> swedish_count.txt
+done
+done
+
+
+for direc in `ls /ebio/abt6_projects9/metagenomic_controlled/data/raw_reads/dc3000_infections`; do
+echo $direc
+my_fil=`ls $direc | grep R2`
+for rec in $my_fil
+do
+num_rec=`zcat $direc/$rec | grep "^@" | wc -l`
+echo $direc/rec, $num_rec >> dc3000_count.txt
+done
+done
+
+for direc in `ls /ebio/abt6_projects9/metagenomic_controlled/data/raw_reads/hpa_infections/controlled_metagenomics`; do
+echo $direc
+my_fil=`ls $direc | grep R2`
+for rec in $my_fil
+do
+num_rec=`zcat $direc/$rec | grep "^@" | wc -l`
+echo $direc/rec, $num_rec >> /ebio/abt6_projects9/metagenomic_controlled/data/raw_reads/hpa_count.txt
+done
+done
+
+for direc in `ls /ebio/abt6_projects9/metagenomic_controlled/data/raw_reads/german_samples`; do
+echo $direc
+my_fil=`ls $direc | grep R2`
+for rec in $my_fil
+do
+num_rec=`zcat $direc/$rec | grep "^@" | wc -l`
+echo $direc/rec, $num_rec >> german_count.txt
+done
+done
